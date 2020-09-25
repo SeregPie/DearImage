d97b3797e3b2282fea0042e6fc6e39bdd0a6cfd0
Sergej Sintschilin <seregpie@gmail.com>
2020-09-25 10:09:21 +0200

    +

--- src/DearImage.from/index.js
+++ src/DearImage.from/index.js
@@ -8,3 +8,14 @@ DearImage.from = function(value) {
 	let context = CanvasRenderingContext2D_from(value);
 	return new this(context);
 };
+
+DearImage.fromExcept = (() => {
+	let warn = true;
+	return function(...args) {
+		if (warn) {
+			console.warn('[DearImage] The function `.fromExcept` is deprecated. Please use `.from` instead.');
+			warn = false;
+		}
+		return this.from.call(this, ...args);
+	};
+})();
--- test.js
+++ test.js
@@ -3,8 +3,9 @@ let {Canvas} = require('canvas');
 let canvas = new Canvas(100, 100);
 
 let ctx = canvas.getContext('2d');
-ctx.font = 'BOLD 10px Arial';
+ctx.font = 'BOLD aaa 10px Arial';
 console.log(ctx.font);
 
-ctx.style = 'rgba(255, 255';
-console.log(ctx.style);
+console.log(ctx.fillStyle);
+ctx.fillStyle = 'rgba(255, 255';
+console.log(ctx.fillStyle);
